ddcbcf55fda6ce58d8aed2e6ccfc5d94e3b9f61d
Ali Imran <126282419+Aliimran1074@users.noreply.github.com>
2023-06-09 22:52:40 +0500

    Add files via upload

---
+++ Cart.js
@@ -0,0 +1,125 @@
+let products = [
+    { id: 1, name: "Bat", price: 6, is_featured: false },
+    { id: 2, name: "Cricket Pads",price: 20, is_featured: false },
+    { id: 3, name: "Football", price: 20, is_featured: false },
+]
+//taking index
+let cart = localStorage.getItem('cart')
+// console.log(cartBodyWrapper)
+let cartArray = cart.split(",")
+for (i = 0; i < cartArray.length; i++) {
+    let cartIndex = cartArray[i][1]
+    // creating cart wrapper div
+    let cartWrapper = document.createElement('DiV')
+    cartWrapper.className = 'cart-title-wrapper'
+    // creating cart body div
+    let cartBody = document.createElement('DIV')
+    cartBody.className = 'cart-body'
+    // appending cartbody to cart wrapper as child
+    cartWrapper.appendChild(cartBody)
+    //creating heading of cart product with h3 element
+    let heading = document.createElement('H3')
+    // creating headingText
+    let headingText = document.createTextNode(products[cartIndex].name)
+    // appending heading text to heading
+    heading.appendChild(headingText)
+    // appending heading to cartbody as child
+    cartBody.appendChild(heading)
+    // creating cart quantity div
+    let cartQuantityDiv = document.createElement('DIV')
+    // giving class of cart-quantity to cartQuantity
+    cartQuantityDiv.className = 'cart-quantity'
+    // creating cart quantity P
+    let cartQuantityP = document.createElement('P')
+    // creating cart quantity text
+    let cartQuantitytext = document.createTextNode(1)
+    // giving cartQuantityP a class
+    cartQuantityP.className = 'quantityValue'
+    // appending cartQuantityText to cartQuantityP as child
+    cartQuantityP.appendChild(cartQuantitytext)
+    // appending cartQuantityP to cartQuantityDiv
+    cartQuantityDiv.appendChild(cartQuantityP)
+    // appending cartQuantity to CartWrapper as child
+    cartWrapper.appendChild(cartQuantityDiv)
+    //creating cartPriceDiv
+    let cartPriceDiv = document.createElement('DIV')
+    // giving classname to cartPriceDiv
+    cartPriceDiv.className = 'cart-price'
+    // creating a p element
+    let cartPriceP = document.createElement('P')
+    // creating cart price text
+    let cartPricetext = document.createTextNode("$" + products[cartIndex].price)
+    // appendind cartPricetext to cartPrice
+    cartPriceP.appendChild(cartPricetext)
+    // appending cartPrice to cartPriceDiv
+    cartPriceDiv.appendChild(cartPriceP)
+    cartWrapper.appendChild(cartPriceDiv)
+   // creating subtotalDiv
+    let subtotalDiv = document.createElement('DIV')
+    // giving classname to subtotalDIv
+    subtotalDiv.className = 'subtotal'
+    //creating subotalp
+    let subtotalP = document.createElement('P')
+    // giving classname to subtotalP
+    subtotalP.className = 'subtotal-p'
+    // saving subtotal price to variable
+    let subtotalprice = products[cartIndex].price * cartQuantityP.innerText
+    // creating subtotal text
+    let subTotalText = document.createTextNode('$' + subtotalprice)
+    // appending subTotalText to subtotalDiv
+    subtotalP.appendChild(subTotalText)
+    // appending subtotalp to  subtotal DIv
+    subtotalDiv.appendChild(subtotalP)
+    // appending subtotalDiv to cart quantity div
+    cartWrapper.appendChild(subtotalDiv)
+    // getting previous subtotal
+    let previousSubTotal = document.getElementsByClassName('subtotal-p')
+    // appending cartwrapper to document
+    let cartDiv = document.getElementById('cart-body-wrapper')
+    cartDiv.appendChild(cartWrapper)
+    document.getElementById('total-price').innerText = Number(document.getElementById('total-price').innerText) + Number(subtotalprice)
+}
+
+let totalWithout$ = document.getElementById('total-price').innerText
+document.getElementById('total-price').innerText = "$" + totalWithout$
+
+let isCartEmpty = localStorage.getItem('cartValue') == null
+document.getElementById("cart-value").innerText = isCartEmpty ? 'Cart:0' : localStorage.getItem('cartValue')
+
+function checkout() {
+    localStorage.removeItem('cart')
+    localStorage.removeItem('cartValue')
+    // if cart is empty
+    if (localStorage.getItem('isLoggedin') == false || localStorage.getItem('isLoggedin') == null) {
+        let loginDiv = document.getElementById('login-page')
+        loginDiv.style.display = 'flex'
+    } else {
+        window.location = "Practice.html"
+    }
+}
+
+function saveLogintolocal() {
+    let fields = document.getElementById('login-page').getElementsByClassName('input')
+    for (let i = 0; i < fields.length; i++) {
+        if (fields[i].value !== '') {
+            localStorage.setItem("isLoggedin", true)
+            let loginDiv = document.getElementById('login-page')
+            loginDiv.style.display = 'none'
+        }
+    }
+}
+
+function loginAnchor() {
+    let loginDiv = document.getElementById('login-page')
+    loginDiv.style.display = 'flex'
+}
+
+function checkRequired() {
+    let fields = document.getElementById('login-page').getElementsByClassName('input')
+    for (let i = 0; i < fields.length; i++) {
+        if (fields[i].value == '') {
+            let alertBlock = document.getElementById('login-page').getElementsByTagName('p')
+            alertBlock[0].style.display = 'flex'
+        }
+    }
+}
---
+++ practice.js
@@ -0,0 +1,159 @@
+let products = [
+  { id: 1, name: "Bat", price: 6, is_featured: false },
+  { id: 2, name: "Cricket Pads",price: 20, is_featured: false },
+  { id: 3, name: "Football", price: 20, is_featured: false },
+]
+for(let product of products){
+
+  if (product.is_featured == true) {
+  let mainDiv=document.getElementById("wrapper")
+  
+  let card = document.createElement('DIV')
+  card.className="carousel-items"
+  mainDiv.appendChild(card)
+  
+  let heading = document.createElement('H3')
+  card.appendChild(heading)
+  
+  let heading_text = document.createTextNode(product.name)
+  heading.appendChild(heading_text)
+  
+  let photo = document.createElement('IMG')
+  photo.src = product.image
+  card.appendChild(photo)  
+
+  let description = document.createElement('P')
+  let description_text = document.createTextNode(product.description)
+  description.appendChild(description_text)
+  card.appendChild(description)
+  
+  let add_to_cart_button = document.createElement("BUTTON")
+  let add_to_cart_button_text = document.createTextNode('add to cart')
+  add_to_cart_button.appendChild(add_to_cart_button_text)
+  add_to_cart_button.addEventListener('click', addToCart)
+  add_to_cart_button.id = product.id
+  card.appendChild(add_to_cart_button)
+}
+}
+
+
+var slideIndex = 0;
+var slides = document.getElementsByClassName("myslide");
+
+function showSlide(n) {
+  if (n < 0) {
+    slideIndex = slides.length - 1;
+  } else if (n >= slides.length) {
+    slideIndex = 0;
+  } else {
+    slideIndex = n;
+  }
+
+  for (var i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[slideIndex].style.display = "block";
+}
+
+function plusSlides(n) {
+  showSlide(slideIndex + n);
+}
+
+showSlide(slideIndex);
+
+
+// Add to cart 
+function addToCart(){
+//  
+
+  // Changing value of cart in navbar
+  // Access cart element
+  // acces cart value
+  // add 1 to cart value
+  // update cart value
+
+
+  // Getting the element with id cart as a string
+  cart = document.getElementById("cart").innerText
+
+  // splitting it to convert it in an array
+  let cartArray = cart.split("")
+
+  //Taking last item of cartArray using .pop() and storing it in lastItemCart
+  let lastItemCart = cartArray.pop()
+
+  // declaring variable updatedCart and storing value of cart
+  const updatedCartvalue = Number(lastItemCart) + 1
+
+  // Taking the remaining array and continating it with updated cart and storing it in the element with id cart
+  let updatedCart = cartArray.join("") + updatedCartvalue
+
+  document.getElementById("cart").innerText = updatedCart
+  localStorage.setItem('cartValue', updatedCart)
+
+
+
+  //     giving value of product to local storage
+
+  var existing = localStorage.getItem('cart');
+
+  // // If no existing data, use the value by itself
+  // // Otherwise, add the new value to it
+  var data = (existing) ? existing + ',' + JSON.stringify(this.id) : JSON.stringify(this.id);
+
+  // // Save back to localStorage
+  localStorage.setItem('cart', data);
+  // let objectId = products[this.id]
+  // localStorage.setItem('cart',Object.entries(objectId))
+
+  // alerting "article added tocart"
+  alert("Article added to cart")
+}
+
+
+function ScrollToFooter(){
+    var footerElement = document.getElementById("footer");
+    footerElement.scrollIntoView();
+}
+function ScrollToMid(){
+  var moveToProduct = document.getElementById("product");
+  moveToProduct.scrollIntoView();      //scrollIntoView(): Pre defined function used to scroll the page
+}
+function validPassword(){
+  var password = document.getElementById("password").value;
+  var numberRegx = /\d/;
+  if(password.length<8 || !numberRegx.test(password)){
+    alert("Password must be at least 8 characters long and contain at least one number.")
+    return false;
+  }
+  else{
+    return true;
+  }
+}
+
+function saveLogintolocal() {
+  let fields = document.getElementById('login-page').getElementsByClassName('input')
+  for (let i = 0; i < fields.length; i++) {
+      if (fields[i].value !== '') {
+          localStorage.setItem("isLoggedin", true)
+          let loginDiv = document.getElementById('login-page')
+          loginDiv.style.display = 'none'
+      }
+  }
+}
+
+function loginAnchor() {
+  let loginDiv = document.getElementById('login-page')
+  loginDiv.style.display = 'flex'
+}
+
+function checkRequired() {
+  let fields = document.getElementById('login-page').getElementsByClassName('input')
+  for (let i = 0; i < fields.length; i++) {
+      if (fields[i].value == '') {
+          let alertBlock = document.getElementById('login-page').getElementsByTagName('p')
+          alertBlock[0].style.display = 'flex'
+      }
+  }
+}
